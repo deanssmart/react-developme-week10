95ea395a117a22999dc6f4c8be5f26d8e4ee42a1
deanssmart <deanssmart@gmail.com>
2020-09-02 12:51:19 +0100

    axios Challenge 2 complete

--- src/ajax.js
+++ src/ajax.js
@@ -1,5 +1,13 @@
 import axios from './axios';
 
-axios.get('/articles').then(({ data } ) => {
-    console.log(data.data);
+// axios.get('/articles').then(({ data }) => {
+//     console.log(data.data);
+// });
+
+axios.post('/articles', {
+    title: "Here we go again",
+    content: "Week 10, lets see how much I remember about API's",
+    tags: ["#post", "#another", "woo"]
+}).then(({ data }) => {
+    console.log(data.data.id);
 });
