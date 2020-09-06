a7ab7d7014a620530f9de87ced4069365b3ff1ad
deanssmart <deanssmart@gmail.com>
2020-09-06 13:45:37 +0100

    all reducer tests passed

--- src/ToDo/reducer.js
+++ src/ToDo/reducer.js
@@ -13,7 +13,6 @@ export const removeItem = (state, { index }) => {
 };
 
 export const updateItem = (state, { index, value }) => {
-
     let items = [...state.items];
 
     items[index] = {
@@ -26,4 +25,28 @@ export const updateItem = (state, { index, value }) => {
         items: items
     };
   
+};
+
+export const completeItem = (state, { index }) => {
+    let items = [...state.items];
+
+    items[index] = {
+        ...state.items[index],
+        completed: true
+    };
+
+    return {
+        ...state,
+        items: items
+    };
+};
+
+export default (state, action) => {
+    switch (action.type) {
+        case "NEW_ITEM": return addItem(state, action);
+        case "REMOVE_ITEM": return removeItem(state, action);
+        case "CHANGE_ITEM": return updateItem(state, action);
+        case "MARK_COMPLETED": return completeItem(state, action);
+        default: return state;
+    }
 };
